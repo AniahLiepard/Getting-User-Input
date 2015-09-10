528fc159f3f6e18799113cbe60117f8bac62843b
AniahLiepard <aniah.garcia@techhigh.us>
2015-09-10 09:45:04 -0400

    Getting User Input Project
    https://www.thenewboston.com/videos.php?cat=31&amp;video=17971

---
+++ ProjectThree.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ProjectThree {
+	public static void main(String args[]){
+		Scanner hotdog = new Scanner(System.in);
+		System.out.println(hotdog.nextLine());
+	}
+}
